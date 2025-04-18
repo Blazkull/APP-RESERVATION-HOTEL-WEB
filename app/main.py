a3d2c4ccd401f23cfaa48ea620f241da6af991c9
JHOAN ACOSTA AMARANTO <159073532+Blazkull@users.noreply.github.com>
2025-04-18 15:52:10 -0500

    Update main.py
    update method cross conection from frontend

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from core.database import create_db_and_tables
 from routers import users, categories, reservations
+from fastapi.middleware.cors import CORSMiddleware # habilitar CROS (conexion con frontend)
 
 app = FastAPI()
 
@@ -11,3 +12,21 @@ def startup():
 app.include_router(users.router)
 app.include_router(categories.router)
 app.include_router(reservations.router)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://mi-dominio.com",
+    "https://otro-dominio.net",
+    "*",  # Permite todos los orígenes (solo para desarrollo, ¡no recomendado para producción!)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, PUT, DELETE, OPTIONS, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
+
