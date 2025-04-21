bea1d5db671d23f67719467938ebdd80b4503856
Rafael Jimenez <raff60116@gmail.com>
2025-04-21 11:44:21 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from core.database import create_db_and_tables
 from routers import usertypes,users,clients,roomtypes,roomstatus,room
 from fastapi.middleware.cors import CORSMiddleware # habilitar CROS (conexion con frontend)
-
+from routes import reservations
 app = FastAPI()
 
 @app.on_event("startup")
@@ -15,7 +15,7 @@ app.include_router(clients.router)
 app.include_router(roomtypes.router)
 app.include_router(roomstatus.router)
 app.include_router(room.router)
-#app.include_router(reservations.router)
+app.include_router(reservations.router)
 
 
 
