803c40225c0024910b692662fde15de955a99b87
JHOANN ENIRQUE ACOSTA AMARANTO <jeacosta37@gmail.com>
2025-04-22 10:33:02 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -43,4 +43,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
