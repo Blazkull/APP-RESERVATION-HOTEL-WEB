22a85909781428880c720b2d767974656416a669
Rafael Jimenez <raff60116@gmail.com>
2025-06-04 16:03:04 -0500

    conex_bd_nube

--- core/security.py
+++ core/security.py
@@ -1,20 +1,28 @@
+# D:\APP-RESERVATION-HOTEL-WEB\core\security.py
 
 from sqlmodel import select
 from core.database import SessionDep
-from models.user import  User
 from typing import Annotated
 from fastapi import Depends
-from fastapi.security import  OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.exceptions import HTTPException
 
 from jose import jwt
 
-from dotenv import load_dotenv
-import os
+# --- INICIO DE LA MODIFICACIÓN ---
+# Elimina estas dos líneas:
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
-SECRET_KEY= os.getenv('SECRET_KEY')
-ALGORITHM= os.getenv('ALGORITHM')
+# Importa tus settings directamente desde core.config
+from core.config import settings
+from models.user import User
+# --- FIN DE LA MODIFICACIÓN ---
+
+
+# Ahora usa settings.SECRET_KEY y settings.ALGORITHM
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
 
 
 #instancia de una clase
@@ -22,24 +30,22 @@ outh2_scheme= OAuth2PasswordBearer(tokenUrl="token")
 
 def encode_token(data:dict):
     token = jwt.encode(data,SECRET_KEY,algorithm=ALGORITHM)
-    return token
-    
+    return token 
+
 def decode_token(token:Annotated[str,Depends(outh2_scheme)], session: SessionDep):
     try:
         data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username = data.get('username')
         if username is None:
             raise HTTPException(status_code=400, detail="The token data is incomplete")
-        
+
         user_db = session.exec(select(User).where(User.username == username)).first()
-        
+
         if user_db is None:
             raise HTTPException(status_code=404, detail="user not found")
         if not user_db.active:
-            raise HTTPException(status_code=403, detail="User  disenable. please, contact your manager system") 
+            raise HTTPException(status_code=403, detail="User  disenable. please, contact your manager system") 
         return
 
     except jwt.JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
-
-
