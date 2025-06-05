f6d66469de7f6a16f5639997567f3685be33744b
Rafael Jimenez <raff60116@gmail.com>
2025-06-04 20:33:09 -0500

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,12 +1,20 @@
-
 from typing import Annotated
 from fastapi import Depends
 from sqlmodel import SQLModel, create_engine, Session
 from core.config import settings
 
+
 engine = create_engine(settings.DATABASE_URL, echo=True)
 
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 def create_db_and_tables():
+
     from models.user import User
     from models.user_type import UserType
     from models.reservation import Reservation
@@ -16,11 +24,3 @@ def create_db_and_tables():
     from models.room_type import RoomType
     from models.room_status import RoomStatus
     from models.token import Token
-
-    SQLModel.metadata.create_all(engine)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-SessionDep = Annotated[Session, Depends(get_session)]
